992ce369d1871d7bf01fd5cb37603a312f134eb5
danil <danil.kr811@gmail.com>
2017-04-22 23:37:26 +0300

    Add post view page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     context = {'posts': posts}
     return render(request, 'blog/post_list.html', context)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context)
